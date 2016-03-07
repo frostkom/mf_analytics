a65313da3c929cd7397715d1bdee32c69ad67e84
frostkom <martin.fors@frostkom.se>
2016-03-07 12:33:52 +0100

    Change formating in settings

--- include/functions.php
+++ include/functions.php
@@ -62,8 +62,8 @@ function setting_analytics_google_callback()
 	$tracking_example = "UA-0000000-0";
 
 	echo "<label>
-		<input type='text' name='setting_analytics_google' value='".$option."' placeholder='".$tracking_example."...'><br>
-		<span class='description'>".__("Login to your account and find the tracking code looking like", 'lang_analytics')." ".$tracking_example."</span>
+		<input type='text' name='setting_analytics_google' value='".$option."' placeholder='".$tracking_example."...'>
+		<p class='description'>".__("Login to your account and find the tracking code looking like", 'lang_analytics')." ".$tracking_example."</p>
 	</label>";
 }
 
@@ -75,7 +75,7 @@ function setting_analytics_clicky_callback()
 
 	echo "<label>
 		<input type='text' name='setting_analytics_clicky' value='".$option."'>" // placeholder='".$tracking_example."...'
-		//."<br><span class='description'>".__("Login to your account and find the tracking code looking like", 'lang_analytics')." ".$tracking_example."</span>"
+		//."<p class='description'>".__("Login to your account and find the tracking code looking like", 'lang_analytics')." ".$tracking_example."</p>"
 	."</label>";
 }
 
@@ -85,7 +85,7 @@ function setting_analytics_save_admin_stats_callback()
 
 	echo "<label>
 		<input type='checkbox' name='setting_analytics_save_admin_stats' value='1' ".checked(1, $option, false).">
-		<span class='description'>".__("Check if you would like to save admin statistics", 'lang_analytics')."</span>
+		<p class='description'>".__("Check if you would like to save admin statistics", 'lang_analytics')."</p>
 	</label>";
 }
 
@@ -96,20 +96,6 @@ function footer_analytics()
 
 	if($setting_analytics_google != '')
 	{
-		/*echo "<script>
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', '".$setting_analytics_google."']);
-			_gaq.push(['_trackPageview']);
-			(function()
-			{
-				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
-		</script>";*/
-
-		//google-analytics.com/urchin.js
-
 		echo "<script src='//google-analytics.com/ga.js'></script>
 		<script>
 			_uacct = '".$setting_analytics_google."';
