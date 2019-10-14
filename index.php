6a3b4209eb93b1910320f47a125a62e143a45acf
frostkom <martin.fors@frostkom.se>
2019-10-14 12:30:51 +0200

    Update translation

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Analytics
 Plugin URI: https://github.com/frostkom/mf_analytics
 Description: 
-Version: 3.1.7
+Version: 3.1.8
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
